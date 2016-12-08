7a94f407e6ce9aae8854d13f74723d49899aca76
alerighi <alerighi4@gmail.com>
2016-12-08 01:42:30 +0100

    Riforme costituzionali al codice

--- src/it/alerighi/spider/Main.java
+++ src/it/alerighi/spider/Main.java
@@ -1,5 +1,7 @@
 package it.alerighi.spider;
 
+import javax.swing.*;
+
 /**
  * Created by ale on 08/12/16.
  */
@@ -7,6 +9,12 @@ public class Main {
     public static void main(String args[]) {
         System.setProperty("apple.awt.application.name", "Spider");
         System.setProperty("apple.laf.useScreenMenuBar", "true");
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         new Spider();
     }
 }
